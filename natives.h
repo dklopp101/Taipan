ab3e6fbba368989a10a056c7192ede706c3af33b
Daniel Klopp <daniel.klopp.1989@gmail.com>
2015-04-20 22:49:55 +0930

    Update natives.h

--- natives.h
+++ natives.h
@@ -12,31 +12,36 @@ typedef struct m_str_t { char mem[STR_MEMSIZE]; } m_str;  /* text */
 /* Machine Instruction Stuff */
 #define instr_opcode (instr[ip].opcode)
 
-#define instr_iop0   (instr[ip].i_op[0])
-#define instr_iop1   (instr[ip].i_op[1])
-#define instr_iop2   (instr[ip].i_op[2])
-#define instr_iop3   (instr[ip].i_op[3])
-#define instr_iop4   (instr[ip].i_op[4])
+#define instr_iop0   (program->code[ip].i_arg[0])
+#define instr_iop1   (program->code[ip].i_arg[1])
+#define instr_iop2   (program->code[ip].i_arg[2])
+#define instr_iop3   (program->code[ip].i_arg[3])
+#define instr_iop4   (program->code[ip].i_arg[4])
 
-#define instr_rop0   (instr[ip].r_op[0])
-#define instr_rop1   (instr[ip].r_op[1])
-#define instr_rop2   (instr[ip].r_op[2])
-#define instr_rop3   (instr[ip].r_op[3])
-#define instr_rop4   (instr[ip].r_op[4])
+#define instr_rop0   (program->code[ip].r_arg[0])
+#define instr_rop1   (program->code[ip].r_arg[1])
+#define instr_rop2   (program->code[ip].r_arg[2])
+#define instr_rop3   (program->code[ip].r_arg[3])
+#define instr_rop4   (program->code[ip].r_arg[4])
 
-#define instr_sop0   (instr[ip].s_op[0])
-#define instr_sop1   (instr[ip].s_op[1])
-#define instr_sop2   (instr[ip].s_op[2])
-#define instr_sop3   (instr[ip].s_op[3])
-#define instr_sop4   (instr[ip].s_op[4])
+#define instr_sop0   (program->code[ip].s_arg[0])
+#define instr_sop1   (program->code[ip].s_arg[1])
+#define instr_sop2   (program->code[ip].s_arg[2])
+#define instr_sop3   (program->code[ip].s_arg[3])
+#define instr_sop4   (program->code[ip].s_arg[4])
 
 #define INSTR_ARG_MAX 5
 
 struct m_instr {
     int  opcode;
-    m_int i_args[INSTR_ARG_MAX];
-    m_real r_args[INSTR_ARG_MAX];
-    m_str s_args[INSTR_ARG_MAX];
+    m_int i_arg[INSTR_ARG_MAX];
+    m_real r_arg[INSTR_ARG_MAX];
+    m_str s_arg[INSTR_ARG_MAX];
+};
+
+struct m_code {
+    struct m_instr* code;
+    unsigned instr_count;
 };
 
 #endif // NATIVES_H_INCLUDED
