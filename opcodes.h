94566d3c8d45943b9ed9b171cde3181b92b2ff9b
Daniel Klopp <daniel.klopp.1989@gmail.com>
2015-05-29 19:47:29 +0930

    Update opcodes.h

--- opcodes.h
+++ opcodes.h
@@ -1,280 +1,211 @@
-#ifndef VM_H_INCLUDED
-#define VM_H_INCLUDED
-
-#include "opcodes.h"
-
-#define PROCESS_RETVAL_INDEX 0
-
-#define istk_top       (istk[isp])
-#define istk_pop()     (istk[isp--])
-#define istk_push(v)   (istk[++isp] = v)
-
-#define fstk_top       (fstk[fsp)
-#define fstk_pop()     (fstk[fsp--])
-#define fstk_push(v)   (fstk[++fsp] = v)
-
-typedef unsigned char byte;
-typedef int t_int;
-typedef double t_float;
-
-#define INT_OPERAND_MAX     6
-#define ISTK_SIZE          40
-#define IREG_SIZE           8
-
-#define FLOAT_OPERAND_MAX   6
-#define FSTK_SIZE          40
-#define FREG_SIZE           8
-
-/// SUBROUTINES.
-#define CALLSTACK_SIZE     20
-
-typedef struct {
-    int stk[CALLSTACK_SIZE];
-    int* frame;
-} callstack_t;
-
-// Initalises callstack pointer, pushes main frame,
-// initalises frame pointer.
-#define init_callstack() cstk.stk[0] = 0;   \
-                         cstk.stk[1] = 0;   \
-                         cstk.stk[2] = 0;   \
-                         cstk.stk[3] = 0;   \
-                         cstk.frame = cstk.stk
-
-// Subroutine Callstack Frames.
-// frame: 0 - frame base (it's index in the callstack).
-//        1 - start instruction (first instruction of subroutine).
-//        2 - return instruction (instruction directly after subroutine call).
-//        3 - exception handler count (counts how many exception handlers in frame).
-//        n - frame base finder.
-//
-// When a subroutine is called it first pushes 0,1,2,3 of above onto the stack,
-// this is the head of the frame, when another subroutine is called from this one,
-// the frame base finder is pushed onto the callstack, then the frame pointer is
-// reset. This allows the previous frame base to be found no matter how many
-// exception handlers or whatever happends inside the stack.
-
-#define frame_base       (cstk.frame[0])
-#define frame_calladdr   (cstk.frame[1])
-#define frame_retaddr    (cstk.frame[2])
-#define frame_hcount     (cstk.frame[3])
-
-/// INSTRUCTIONS.
-#define iop(n) ((prog[ip]).iop[n])
-#define fop(n) ((prog[ip]).fop[n])
-
-#define execute_next(instr) goto *optable[(instr).opcode]
-
-typedef struct {
-    byte opcode;
-    t_int iop[INT_OPERAND_MAX];
-    t_float fop[FLOAT_OPERAND_MAX];
-} instr_t;
-
-
-/// VM FUNCTION.
-int tprocess(instr_t* __restrict__ prog)
-{
-    build_optable();
-
-    callstack_t cstk;
-    init_callstack();
-
-    register int isp = -1;
-    register int fsp = -1;
-    register int csp = 0;
-    register int ip  = 0;
-    register t_int iop_left;
-    register t_int iop_right;
-    register t_float fop_left;
-    register t_float fop_right;
-    register char boolflag = 0;
-
-    t_int ireg[IREG_SIZE];
-    t_float freg[FREG_SIZE];
-
-    t_int istk[ISTK_SIZE];
-    t_float fstk[FSTK_SIZE];
-
-
-
-
-
-    execute_next(prog[ip]);
-
-
-    jump:
-        ip = iop(0);
-        execute_next(prog[ip]);
-
-    jump_t:
-        if (boolflag == 1)
-            ip = iop(0);
-        else
-            ++ip;
-        execute_next(prog[ip]);
-
-
-    jump_f:
-        if (boolflag == 1)
-            ip = iop(0);
-        else
-            ++ip;
-        execute_next(prog[ip]);
-
-    call:
-        // Finalise the current frame then set up the new one.
-        ++(csp);
-        cstk.stk[csp] = frame_base; // push the current frame's finder index.
-
-        ++(csp);
-        cstk.stk[csp] = csp + 1; // push the new frame's base(it's index in the callstack).
-
-        cstk.frame = cstk.stk + csp; // officiate the new frame by setting current frame.
-
-        ++(csp);
-        cstk.stk[csp] = iop(0); // push the index of the subr being called.
-
-        ++(csp);
-        cstk.stk[csp] = ip + 1; // push the index of the frame's return instruction.
-
-        ++(csp);
-        cstk.stk[csp] = 0; // push 0 to reserve a spot for the frame's exception handler counter.
-
-        ip = frame_calladdr;
-        execute_next(prog[ip]);
-
-    leave:
-        // Firstly set ip to the frame's return instruction.
-        ip = frame_retaddr;
-
-        // Exit the current frame to the next outer one by
-        // jumping to the outer frame's finder. Then setting
-        // the frame pointer to said frame, then decrement
-        // csp because the finder isnt needed anymore.
-        csp = frame_base;
-
-        cstk.frame = cstk.stk + cstk.stk[csp];
-        --(csp);
-
-        // Jump to the frame's return instruction.
-        execute_next(prog[ip]);
-
-    i_const:
-        istk_push(iop(0));
-        execute_next(prog[++ip]);
-
-    i_pop:
-        --isp;
-        execute_next(prog[++ip]);
-
-    // Integer Logic.
-    i_eql:
-        boolflag = istk_pop() == istk_pop();
-        execute_next(prog[++ip]);
-
-    i_neql:
-        boolflag = istk_pop() == istk_pop();
-        execute_next(prog[++ip]);
-
-    i_gt_eql:
-        boolflag = istk_pop() == istk_pop();
-        execute_next(prog[++ip]);
-
-
-    i_lt_eql:
-        boolflag = istk_pop() == istk_pop();
-        execute_next(prog[++ip]);
-
-    i_gt:
-        boolflag = istk_pop() == istk_pop();
-        execute_next(prog[++ip]);
-
-    i_lt:
-        boolflag = istk_pop() == istk_pop();
-        execute_next(prog[++ip]);
-
-    i_and:
-        boolflag = istk_pop() == istk_pop();
-        execute_next(prog[++ip]);
-
-    i_or:
-        boolflag = istk_pop() == istk_pop();
-        execute_next(prog[++ip]);
-
-    i_not:
-        boolflag = !istk_pop();
-        execute_next(prog[++ip]);
-
-
-    // Integer Arithmetic.
-    i_add:
-        istk_top = istk_pop() + istk_top;
-        execute_next(prog[++ip]);
-
-    i_sub:
-        istk_top = istk_pop() - istk_top;
-        execute_next(prog[++ip]);
-
-    i_mul:
-        istk_top = istk_pop() * istk_top;
-        execute_next(prog[++ip]);
-
-    i_div:
-        istk_top = istk_pop() / istk_top;
-        execute_next(prog[++ip]);
-
-    i_mod:
-        istk_top = istk_pop() % istk_top;
-        execute_next(prog[++ip]);
-
-    i_inv:
-        istk_top = ~istk_top;
-        execute_next(prog[++ip]);
-
-    i_inc:
-        ++istk_top;
-        execute_next(prog[++ip]);
-
-    i_dec:
-        --istk_top;
-        execute_next(prog[++ip]);
-
-
-
-
-    die:
-        return ireg[PROCESS_RETVAL_INDEX];
-
-
-    // VM Debugging.
-    dump_istk:
-        printf("\nINTEGER STACK DUMP:\nisp: %d\n\n", isp);
-        for (iop_left=0; iop_left < ISTK_SIZE; ++iop_left)
-            printf("[%d] : %d\n", iop_left, istk[iop_left]);
-
-        ++ip;
-        execute_next(prog[ip]);
-
-    dump_istktop:
-        printf("\ninteger stack top: %d\n", istk[isp]);
-        ++ip;
-        execute_next(prog[ip]);
-
-    dump_boolflag:
-        printf("\nBOOL FLAG: %d\n", boolflag);
-
-        ++ip;
-        execute_next(prog[ip]);
-
-    dump_cstk:
-        printf("\nCALL STACK DUMP:\ncsp: %d\n\n", csp);
-
-        for (iop_left=0; iop_left < CALLSTACK_SIZE; ++iop_left)
-            printf("[%d] : %d\n", iop_left, cstk.stk[iop_left]);
-
-        ++ip;
-        execute_next(prog[ip]);
-}
-
+#ifndef OPCODES_H_INCLUDED
+#define OPCODES_H_INCLUDED
+
+#define JUMP             0
+#define JUMP_EQL         1
+#define JUMP_NEQL        2
+#define JUMP_GT_EQL      3
+#define JUMP_LT_EQL      4
+#define JUMP_GT          5
+#define JUMP_LT          6
+
+#define REG_JUMP         7
+#define REG_JUMP_T       8
+#define REG_JUMP_F       9
+
+#define LOOP_TIMES      10
+#define LOOP_WHILE      11
+#define LOOP_UNTIL      12
+
+#define CALL            13
+#define LEAVE           14
+
+#define IREG_LOAD_C     15
+#define IREG1_LOAD_C    16
+#define IREG2_LOAD_C    17
+#define IREG3_LOAD_C    18
+#define IREG4_LOAD_C    19
+#define IREG5_LOAD_C    20
+#define IREG6_LOAD_C    21
+
+#define IREG_LOAD_S     22
+#define IREG1_LOAD_S    23
+#define IREG2_LOAD_S    24
+#define IREG3_LOAD_S    25
+#define IREG4_LOAD_S    26
+#define IREG5_LOAD_S    27
+#define IREG6_LOAD_S    28
+
+#define IREG_SWAP       29
+#define IREG_SWAP_S     30
+
+#define IREG_EQL        31
+#define IREG_NEQL       32
+#define IREG_GT_EQL     33
+#define IREG_LT_EQL     34
+#define IREG_GT,        35
+#define IREG_LT,        36
+#define IREG_AND,       37
+#define IREG_OR,        38
+#define IREG_NOT,       39
+#define IREG_AND_BW     40
+#define IREG_OR_BW      41
+#define IREG_XOR_BW     42
+#define IREG_NOT_BW     43
+#define IREG_LSHIFT_BW  44
+#define IREG_RSHIFT_BW  45
+
+#define IREG_INC        46
+#define IREG_DEC        47
+#define IREG_ADD        48
+#define IREG_SUB        49
+#define IREG_MUL        50
+#define IREG_DIV        51
+#define IREG_MOD        52
+
+#define ISTK_PUSH_C     53
+
+#define ISTK_PUSH_R     54
+#define ISTK_PUSH_R1    55
+#define ISTK_PUSH_R2    56
+#define ISTK_PUSH_R3    57
+#define ISTK_PUSH_R4    58
+#define ISTK_PUSH_R5    59
+#define ISTK_PUSH_R6    60
+
+#define ISTK_POP        61
+#define ISTK_SWAP       62
+#define ISTK_RESET      63
+
+#define ISTK_LOAD_C     64
+#define ISTK_LOAD_R     65
+#define ISTK_LOAD_R1    66
+#define ISTK_LOAD_R2    67
+#define ISTK_LOAD_R3    68
+#define ISTK_LOAD_R4    69
+#define ISTK_LOAD_R5    70
+#define ISTK_LOAD_R6    71
+
+#define ISTK_EQL        72
+#define ISTK_NEQL       73
+#define ISTK_GT_EQL     74
+#define ISTK_LT_EQL     75
+#define ISTK_GT         76
+#define ISTK_LT         77
+#define ISTK_AND        78
+#define ISTK_OR         79
+#define ISTK_NOT        80
+
+#define ISTK_INC        81
+#define ISTK_DEC        82
+#define ISTK_ADD        83
+#define ISTK_SUB        84
+#define ISTK_MUL        85
+#define ISTK_DIV        86
+#define ISTK_MOD        87
+#define ISTK_INV        88
+
+#define DIE             89
+
+#define DUMP_ISTK       90
+#define DUMP_ISTKTOP    91
+#define DUMP_CSTK       92
+
+
+#define build_optable() static void* optable[255] = { \
+                                &&jump,               \
+                                &&jump_eql,           \
+                                &&jump_neql,          \
+                                &&jump_gt_eql,        \
+                                &&jump_lt_eql,        \
+                                &&jump_gt,            \
+                                &&jump_lt,            \
+                                &&reg_jump,           \
+                                &&reg_jump_t,         \
+                                &&reg_jump_f,         \
+                                &&loop_times,         \
+                                &&loop_while,         \
+                                &&loop_until,         \
+                                &&call,               \
+                                &&leave,              \
+                                &&ireg_load_c,        \
+                                &&ireg1_load_c,       \
+                                &&ireg2_load_c,       \
+                                &&ireg3_load_c,       \
+                                &&ireg4_load_c,       \
+                                &&ireg5_load_c,       \
+                                &&ireg6_load_c,       \
+                                &&ireg_load_s,        \
+                                &&ireg1_load_s,       \
+                                &&ireg2_load_s,       \
+                                &&ireg3_load_s,       \
+                                &&ireg4_load_s,       \
+                                &&ireg5_load_s,       \
+                                &&ireg6_load_s,       \
+                                &&ireg_swap,          \
+                                &&ireg_swap_s,        \
+                                &&ireg_eql,           \
+                                &&ireg_neql,          \
+                                &&ireg_gt_eql,        \
+                                &&ireg_lt_eql,        \
+                                &&ireg_gt,            \
+                                &&ireg_lt,            \
+                                &&ireg_and,           \
+                                &&ireg_or,            \
+                                &&ireg_not,           \
+                                &&ireg_AND_BW,        \
+                                &&ireg_OR_BW,         \
+                                &&ireg_XOR_BW,        \
+                                &&ireg_NOT_BW,        \
+                                &&ireg_LSHIFT_BW,     \
+                                &&ireg_LRIGHT_BW,     \
+                                &&ireg_inc,           \
+                                &&ireg_dec,           \
+                                &&ireg_add,           \
+                                &&ireg_sub,           \
+                                &&ireg_mul,           \
+                                &&ireg_div,           \
+                                &&ireg_mod,           \
+                                &&istk_push_c,        \
+                                &&istk_push_r,        \
+                                &&istk_push_r1,       \
+                                &&istk_push_r2,       \
+                                &&istk_push_r3,       \
+                                &&istk_push_r4,       \
+                                &&istk_push_r5,       \
+                                &&istk_push_r6,       \
+                                &&istk_pop,           \
+                                &&istk_pop_r,         \
+                                &&istk_swap,          \
+                                &&istk_reset,         \
+                                &&istk_load_c,        \
+                                &&istk_load_r,        \
+                                &&istk_load_r1,       \
+                                &&istk_load_r2,       \
+                                &&istk_load_r3,       \
+                                &&istk_load_r4,       \
+                                &&istk_load_r5,       \
+                                &&istk_load_r6,       \
+                                &&istk_eql,           \
+                                &&istk_neql,          \
+                                &&istk_gt_eql,        \
+                                &&istk_lt_eql,        \
+                                &&istk_gt,            \
+                                &&istk_lt,            \
+                                &&istk_and,           \
+                                &&istk_or,            \
+                                &&istk_not,           \
+                                &&istk_inc,           \
+                                &&istk_dec,           \
+                                &&istk_add,           \
+                                &&istk_sub,           \
+                                &&istk_mul,           \
+                                &&istk_div,           \
+                                &&istk_mod,           \
+                                &&istk_inv,           \
+                                &&die,                \
+                                &&dump_istk,          \
+                                &&dump_istktop,       \
+                                &&dump_cstk           \
+                        }
 #endif
