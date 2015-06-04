59bafd9d98335a3c892ca176de1f17e0b1a2416a
Daniel Klopp <daniel.klopp.1989@gmail.com>
2015-06-04 22:45:53 +0930

    Create typedefs.h

---
+++ typedefs.h
@@ -0,0 +1,16 @@
+#ifndef TYPEDEFS_H_INCLUDED
+#define TYPEDEFS_H_INCLUDED
+
+typedef unsigned char byte;
+typedef int t_int;
+typedef double t_float;
+
+/* Size Definitions. */
+#define IREG_SIZE  16
+#define FREG_SIZE  16
+#define CSTK_SIZE 100
+#define ISTK_SIZE 100
+#define FSTK_SIZE 100
+
+
+#endif
