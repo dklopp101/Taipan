34c9facb686d3151271c7546fb2e2c985d5c2cf0
Daniel Klopp <daniel.klopp.1989@gmail.com>
2015-09-06 13:50:54 +0930

    Update error.h

--- error.h
+++ error.h
@@ -1,102 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef error_h_included
+#define error_h_included
 
-#include "error.h"
 #include "lex.h"
 
-char* error_strmap[2] = {"token error", 
-                         "semantic error"};
+#define TOKEN_ERROR    0
+#define SEMANTIC_ERROR 1
 
+char* error_strmap[2];
 
-error_t* alloc_error()
-{
-	void* malloc_check = malloc(sizeof(error_t));
-	error_t* error;
+typedef struct {
+	int num;
+	int type;
+	token_t* token;
+} error_t;
 
-	if (malloc_check) {
-		error = (error_t*) malloc_check;
-		error->type  = -1;
-		error->token =  0;	
-	} else {
-		fprintf(stderr, "alloc_error() malloc call returned NULL.\n");
-		exit(EXIT_FAILURE);
-	}
+typedef struct {
+	error_t* tbl[ERRTBL_SIZE];
+	size_t   count;
+} errtbl_t;
 
-	return error;
-}
+error_t* alloc_error();
+void     free_error(error_t* error);
+error_t* alloc_errtbl();
+void     free_errtbl(errtbl_t* errtbl);
+void     new_error(errtbl_t* errtbl, const int type, token_t* token);
+void     print_error(error_t* error);
+void     print_errtbl(errtbl_t* errtbl);
 
+#endif#ifndef error_h_included
+#define error_h_included
 
-void free_error(error_t* error)
-{
-	free(error);
-}
+#include "lex.h"
 
+#define TOKEN_ERROR    0
+#define SEMANTIC_ERROR 1
 
-error_t* alloc_errtbl()
-{
-	void* malloc_check = malloc(sizeof(errtbl_t));
-	errtbl_t* errtbl;
+char* error_strmap[2];
 
-	if (malloc_check) {
-		errtbl = (errtbl_t*) malloc_check;
-		errtbl->count = 0;	
-	} else {
-		fprintf(stderr, "alloc_errtbl() malloc call returned NULL.\n");
-		exit(EXIT_FAILURE);
-	}
+typedef struct {
+	int num;
+	int type;
+	token_t* token;
+} error_t;
 
-	return errtbl;
-}
+typedef struct {
+	error_t* tbl[ERRTBL_SIZE];
+	size_t   count;
+} errtbl_t;
 
+error_t* alloc_error();
+void     free_error(error_t* error);
+error_t* alloc_errtbl();
+void     free_errtbl(errtbl_t* errtbl);
+void     new_error(errtbl_t* errtbl, const int type, token_t* token);
+void     print_error(error_t* error);
+void     print_errtbl(errtbl_t* errtbl);
 
-
-void free_errtbl(errtbl_t* errtbl)
-{
-	int i;
-
-	for (i=0; i < errtbl->count; ++i)
-		free(errtbl->tbl[i]);
-	
-	free(errtbl->tbl);
-	free(errtbl);
-}
-
-
-
-void new_error(errtbl_t* errtbl, const int type, token_t* token)
-{
-	error_t* error = alloc_error();
-	error->type  = type;
-	error->token = token;
-	(errtbl->tbl)[errtbl->count] = error;
-	++(errtbl->count);
-}
-
-
-
-/* At this point in time print_error() simply prints the offending token, line, col
-   but not much more. When i get around to it this needs to be updated to print the
-   entire line and then an arrow underneith like python does to show clearly where
-   the error is. */
-void print_error(error_t* error)
-{
-	fprintd(stdout, "\n error on line %d at column %d: %s - Offending Token: %s\n",
-		(int) error->token->line, 
-        (int) error->token->col, 
-        error_strmap[error->type], 
-        error->token->buf);
-}
-
-
-
-void print_errtbl(errtbl_t* errtbl)
-{
-	int i;
-
-	if (errtbl->count) {
-		for (i=0; i < errtbl->count; ++i)
-			print_error(errtbl->tbl[i]);
-	}
-
-	fprintf(stdout, "total errors: %d\n", (int) errtbl->count);
-}
+#endif
